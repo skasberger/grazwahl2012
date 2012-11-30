627d331be3e6230207ea998eba5e9761ac0debbb
Stefan Kasberger <stefan.kasberger@alpine-geckos.at>
2012-11-30 15:37:37 +0100

    update R files

--- grazwahl2012_prepdata.R
+++ grazwahl2012_prepdata.R
@@ -13,6 +13,9 @@
 #   												 
 ######################################################
 
+
+##### FUNCTIONS #####
+
 # function to divide bezirk from sprengel in the dataframe
 split_sprengel <- function(data) {
   data$sprengel <- add_zero(data$sprengel)
@@ -22,6 +25,7 @@ split_sprengel <- function(data) {
   data
 }
 
+# function to add homogenize the lenght of sprengel column to 4 letters for easier processing later
 add_zero <- function(data) {
   for(i in seq_along(data)) {
     if(as.integer(data[i]) < 999) {
@@ -31,19 +35,30 @@ add_zero <- function(data) {
   data
 }
 
+
+##### INIT VARIABLES #####
+
+count_disctricts <- 17
+id_districts <- c(1:count_disctricts, "27")
+
+
 ##### GEMEINDERATSWAHLEN #####
+
+# Gemeinderatswahl 2003
 graz_grw2003_bez <- read.csv2("raw/Graz/2003_grw_sprengelbezerg.csv", fileEncoding = "iso-8859-1")
 graz_grw2003_bez$beznr <- graz_grw2003_bez$beznr / 100
 graz_grw2003_spr <- read.csv2("raw/Graz/2003_grw_sprengelerg.csv", fileEncoding = "iso-8859-1")
 graz_grw2003_spr <- graz_grw2003_spr[, 1:9]
 graz_grw2003_spr <- split_sprengel(graz_grw2003_spr)
 
+# Gemeinderatswahl 2008
 graz_grw2008_bez <- read.csv2("raw/Graz/2008_grw_sprengelbezerg.csv", fileEncoding = "iso-8859-1")
 graz_grw2008_bez$beznr <- graz_grw2008_bez$beznr / 100
 graz_grw2008_spr <- read.csv2("raw/Graz/2008_grw_sprengelerg.csv", fileEncoding = "iso-8859-1")
 graz_grw2008_spr <- graz_grw2008_spr[,1:9]
-graz_grw2008_spr <- split_sprengel(graz_grw2008_spr)
+graz_grw2008_spr <-split_sprengel(graz_grw2008_spr)
 
+# Gemeinderatswahl 2012
 graz_grw2012_bez <- read.csv2("raw/Graz/GRW2012_Sprengelbezerg.csv", fileEncoding = "iso-8859-1")
 graz_grw2012_bez$beznr <- graz_grw2012_bez$beznr / 100
 graz_grw2012_spr <- read.csv2("raw/Graz/GRW2012_Sprengelerg.csv", fileEncoding = "iso-8859-1")
@@ -54,25 +69,32 @@ graz_grw2012_wb <- split_sprengel(graz_grw2012_wb)
 
 
 ##### LANDTAGSSWAHLEN #####
+
+# Landtagswahl 2005
 graz_ltw2005_bez <- read.csv2("raw/Graz/2005_ltw_sprengelbezerg.csv", fileEncoding = "iso-8859-1")
 graz_ltw2005_bez$beznr <- graz_ltw2005_bez$beznr / 100
 graz_ltw2005_spr <- read.csv2("raw/Graz/2005_ltw_sprengelerg.csv", fileEncoding = "iso-8859-1")
 graz_ltw2005_spr <- graz_ltw2005_spr[, 1:9]
 graz_ltw2005_spr <- split_sprengel(graz_ltw2005_spr)
 
+# Landtagswahl 2010
 graz_ltw2010_bez <- read.csv2("raw/Graz/ltw_2010_sprengelbezerg.csv", fileEncoding = "iso-8859-1")
 graz_ltw2010_bez$beznr <- graz_ltw2010_bez$beznr / 100
 graz_ltw2010_spr <- read.csv2("raw/Graz/ltw_2010_sprengelerg.csv", fileEncoding = "iso-8859-1")
 graz_ltw2010_wb <- read.csv2("raw/Graz/ltw_2010_sprengel_wahlbe.csv")
 graz_ltw2010_spr <- split_sprengel(graz_ltw2010_spr)
 
+
 ##### NATIONALRATSWAHLEN #####
+
+# Nationalratswahl 2006
 graz_nrw2006_bez <- read.csv2("raw/Graz/2006_nrw_sprengelbezerg.csv", fileEncoding = "iso-8859-1")
 graz_nrw2006_bez$beznr <- graz_nrw2006_bez$beznr / 100
 graz_nrw2006_spr <- read.csv2("raw/Graz/2006_nrw_sprengelerg.csv", fileEncoding = "iso-8859-1")
 graz_nrw2006_spr <- graz_nrw2006_spr[, 1:9]
 graz_nrw2006_spr <- split_sprengel(graz_nrw2006_spr)
 
+# Nationalratswahl 2008
 graz_nrw2008_bez <- read.csv2("raw/Graz/2008_nrw_sprengelbezerg.csv", fileEncoding = "iso-8859-1")
 graz_nrw2008_bez$beznr <- graz_nrw2008_bez$beznr / 100
 graz_nrw2008_spr <- read.csv2("raw/Graz/2008_nrw_sprengelerg.csv", fileEncoding = "iso-8859-1")
--- grazwahl2012_qgis.R
+++ grazwahl2012_qgis.R
@@ -14,44 +14,82 @@
 ######################################################
 
 
+# FIRST: load the data from "grazwahl2012_elections.RData". Just uncomment the next line. 
+# load("grazwahl2012_elections.RData")
+# You also can generate the data newly via executing the grazwahl2012_prepdata file
 
-# FIRST: laod the data from "grazwahl2012_elections.RData"
+##### GEMEINDERATSWAHLEN GRAZ 2012 #####
 
-##### GEMEINDERATSWAHLEN #####
-# Graz 2012
-
-# INIT DATA 
-bezirke <- (c("Innere Stadt", "St Leonhard", "Geidorf", "Lend", "Gries", 
+# INIT VARIABLES
+disctricts <- (c("Innere Stadt", "St Leonhard", "Geidorf", "Lend", "Gries", 
               "Jakomini", "Liebenau", "St.Peter", "Waltendorf", "Ries", "Mariatrost", 
               "Andritz", "Gösting", "Eggenberg", "Wetzelsdorf", "Straßgang", "Puntigam", "Virtuell"))
 
-count_parties <- 11
-count_bezirke <- 17
-id_bezirke <- c(1:count_bezirke, "27")
+count_parties_2012 <- 11
+
+# PREPARE DATA FOR QGIS
+
+# generate list of parties
+graz_grw2012_parties_2012 <- as.character(graz_grw2012_bez$ptname[1:count_parties_2012])
+graz_grw2012_parties_2012[9] <- "EKW"
+#qgis_graz_grw2012_bez_total <- 
+
+# generate matrix of elections participation (with gender specific data)
+wb_bez_total <- tapply(graz_grw2012_wb$wahlbe_gesamt, graz_grw2012_wb$bezirk, sum)
+wb_bez_gender_total <- cbind(tapply(graz_grw2012_wb$wahlbe_mann, graz_grw2012_wb$bezirk, sum),
+                             tapply(graz_grw2012_wb$wahlbe_frau, graz_grw2012_wb$bezirk, sum))
+colnames(wb_bez_gender_total) <- c("men_valid", "women_valid")
 
 
-parties_2012 <- as.character(graz_grw2012_bez$ptname[1:count_parties])
-parties_2012[9] <- "EKW"
-qgis_graz_grw2012_bez <- as.data.frame(id_bezirke)
-
-men <- tapply(graz_grw2012_wb$wahlbe_mann, graz_grw2012_wb$bezirk, sum)
-women <- tapply(graz_grw2012_wb$wahlbe_frau, graz_grw2012_wb$bezirk, sum)
-
-temp_parties <- data.frame(1:count_parties)
-for(i in seq_along(1:count_bezirke)) {
-  start <- (i - 1) * count_parties + 1
-  end <- i * count_parties
+# transform votes organized by party and disctrict to the structure necessary for QGIS
+temp_parties <- data.frame(1:count_parties_2012)
+for(i in seq_along(1:count_disctricts)) {
+  start <- (i - 1) * count_parties_2012 + 1
+  end <- i * count_parties_2012
   temp_parties <- cbind(temp_parties, graz_grw2012_bez$stimmen[start:end])
 }
 temp_parties <- temp_parties[, 2:length(temp_parties)]
 temp_parties <- t(temp_parties)
 temp_parties <- rbind(temp_parties, "NA")
-qgis_graz_grw2012_bez <- cbind(qgis_graz_grw2012_bez, men, women, temp_parties)
-colnames(qgis_graz_grw2012_bez) <- c("id_bezirke", mann", "frau", parties_2012[1:count_parties])
+colnames(temp_parties) <- graz_grw2012_parties_2012[1:count_parties_2012]
 
-write.csv(qgis_graz_grw2012_bez, "QGIS/qgis_graz_grw2012_bez_comma.csv", fileEncoding = "UTF-8")
-write.csv2(qgis_graz_grw2012_bez, "QGIS/qgis_graz_grw2012_bez_semicolon.csv", fileEncoding = "UTF-8")
-rm(temp_parties, men, women, end, start, i)
+
+# PARTICIPATION (total & relative)
+
+# participation overall -> total
+qgis_graz_grw2012_bez_part_total <- cbind(as.data.frame(id_districts), wb_bez_total, wb_bez_gender_total)
+
+write.csv(qgis_graz_grw2012_bez_participation_total, 
+          "QGIS/qgis_graz_grw2012_bez_participation_total_comma.csv", fileEncoding = "UTF-8")
+write.csv2(qgis_graz_grw2012_bez_participation_total, 
+           "QGIS/qgis_graz_grw2012_bez_participation_total_semicolon.csv", fileEncoding = "UTF-8")
+
+# participation overall -> relative
+# qgis_graz_grw2012_bez_participation_relativ <- 
+# colnames(qgis_graz_grw2012_bez_participation_total) <- c("id_district", "man", "woman", graz_grw2012_parties_2012[1:count_parties])
+#write.csv(qgis_graz_grw2012_bez_participation_relativ, 
+          #"QGIS/qgis_graz_grw2012_bez_participation_relativ_comma.csv", fileEncoding = "UTF-8")
+#write.csv2(qgis_graz_grw2012_bez_participation_relativ, 
+#           "QGIS/qgis_graz_grw2012_bez_participation_relativ_semicolon.csv", fileEncoding = "UTF-8")
+
+# PARTIES (total & relative)
+
+# votings for parties by district -> total
+qgis_graz_grw2012_bez_parties_total <- cbind(as.data.frame(id_districts), temp_parties)
+write.csv(qgis_graz_grw2012_bez_parties_total, 
+          "QGIS/qgis_graz_grw2012_bez_parties_total_comma.csv", fileEncoding = "UTF-8")
+write.csv2(qgis_graz_grw2012_bez_parties_total, 
+           "QGIS/qgis_graz_grw2012_bez_parties_total_semicolon.csv", fileEncoding = "UTF-8")
+
+# votings for parties by district -> relative
+# qgis_graz_grw2012_bez_parties_relativ <- 
+#write.csv(qgis_graz_grw2012_bez_parties_relativ, 
+#          "QGIS/qgis_graz_grw2012_bez_parties_relativ_comma.csv", fileEncoding = "UTF-8")
+#write.csv2(qgis_graz_grw2012_bez_parties_relativ, 
+#           "QGIS/qgis_graz_grw2012_bez_parties_relativ_semicolon.csv", fileEncoding = "UTF-8")
+
+# remove unneccessary data
+rm(temp_parties, start, end, i)
 
 
 
