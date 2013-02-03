6e726127daf7405db3b70c93c26fd0a2e330724f
Stefan Kasberger <stefan.kasberger@alpine-geckos.at>
2013-02-03 17:28:07 +0100

    fixed bug: absolute votes for calculating relative votes where all votes, not only valid votes. So the unvalid votes amount were always missing. updated file organization with load and save functions

--- README.md
+++ README.md
@@ -2,3 +2,10 @@ dataanalyses-grazwahl2012
 =========================
 
 Datenanalyse und Visualisierung Gemeinderatswahlen in Graz 2012
+
+# BESCHREIBUNG
+Die Analyse soll einem einen kleinen Einblick in das Ergebnis der Grazer Gemeinderatswahlen 2012 geben. Im Zuge des Arbeiten mit Datensätzen zu Wahlen soll den spezifischen Fragestellungen nachgegangen und diese visual dargestellt werden.
+Dies ist in erster Linie das Visualisieren des Ergebnisses auf einer Karte der Stadt Graz sowie Erzeugen unterschiedlicher Säulendiagramme zu den einzelnen Bezirken.
+Weiters soll auch eine Analyse von Graz spezifischen Fragestellungen in Verbindung sozio-ökonomischen Daten (sofern vorhanden) gemacht und nach räumlichen Mustern gesucht werden.
+
+[Project Webpage](http://openscience.alpine-geckos.at/projects/grazwahlen-2012/)
--- prepare_data_grw2012.R
+++ prepare_data_grw2012.R
@@ -7,7 +7,7 @@
 #   Date: 29.11.2012
 #   Version: 1.0
 #	  Language: 2.15.2
-#   Software: 0.96.330
+#   Software: RStudio 0.97.311
 #   License: FreeBSD (2-clause BSD)
 #   												 
 ######################################################
@@ -32,48 +32,57 @@ source("functions.R")
 
 # Initialize Variables
 nameDistricts <- (c("Innere Stadt", "St. Leonhard", "Geidorf", "Lend", "Gries", 
-                "Jakomini", "Liebenau", "St. Peter", "Waltendorf", "Ries", "Mariatrost", 
-                "Andritz", "Gösting", "Eggenberg", "Wetzelsdorf", "Straßgang", "Puntigam"))
+                    "Jakomini", "Liebenau", "St. Peter", "Waltendorf", "Ries", "Mariatrost", 
+                    "Andritz", "Gösting", "Eggenberg", "Wetzelsdorf", "Straßgang", "Puntigam"))
 numDistricts <- length(nameDistricts)
 numVotDistricts <- numDistricts + 1
 numberParties <- 11L
-
+eleData <- list(numDistricts, numVotDistricts, numberParties)
+names(eleData) <- c("numDistricts", "numVotDistricts", "numberParties")
+rm(numDistricts, numVotDistricts, numberParties)
+  
 workDir <- getwd()
 folderRaw <- paste0(workDir, "/data/raw/grw2012")
 folderSpatialRaw <- paste0(workDir, "/data/raw/spatial")
 
 # column names
-colNameAllVotesAbs <- "stimmen_abs"
-colNameValidVotesAbs <- "stimmen_gueltig_abs"
-colNameValidVotesRel <- "stimmen_gueltig_rel"
-colNameUnvalidVotesAbs <- "stimmen_ungueltig_abs"
-colNameUnvalidVotesRel <- "stimmen_ungueltig_rel"
-colNameAuthVotersAbs <- "wahlber_alle_abs"
-colNameAuthFemaleVotersAbs <- "wahlber_Frauen_abs"
-colNameAuthFemaleVotersRel <- "wahlber_Frauen_rel"
-colNameAuthMaleVotersAbs <- "wahlber_Maenner_abs"
-colNameAuthMaleVotersRel <- "wahlber_Maenner_rel"
-colNameListplace <- "Listenplatz"
-colNameDistrict <- "bezirk"
-colNameParish <- "sprengel"
-colNameParishDistrict <- "bezirk_sprengel"
-colNamePartyShortname <- "partei_kuerzel"
-colNamePartyName <- "parteiname"
-colNameVotes <- "stimmen"
+allVotesAbs <- "stimmen_abs"
+validVotesAbs <- "stimmen_gueltig_abs"
+validVotesRel <- "stimmen_gueltig_rel"
+unvalidVotesAbs <- "stimmen_ungueltig_abs"
+unvalidVotesRel <- "stimmen_ungueltig_rel"
+authVotersAbs <- "wahlber_alle_abs"
+authFemaleVotersAbs <- "wahlber_Frauen_abs"
+authFemaleVotersRel <- "wahlber_Frauen_rel"
+authMaleVotersAbs <- "wahlber_Maenner_abs"
+authMaleVotersRel <- "wahlber_Maenner_rel"
+listplace <- "Listenplatz"
+district <- "bezirk"
+parish <- "sprengel"
+parishDistrict <- "bezirk_sprengel"
+partyShortname <- "partei_kuerzel"
+partyName <- "parteiname"
+votes <- "stimmen"
+colNames <- list(allVotesAbs, validVotesAbs, validVotesRel, unvalidVotesAbs, unvalidVotesRel, 
+                 authVotersAbs, authMaleVotersRel, authMaleVotersAbs, authFemaleVotersRel, authFemaleVotersAbs,
+                 listplace, district, parish, parishDistrict, partyName, partyShortname, votes)
+rm(allVotesAbs, validVotesAbs, validVotesRel, unvalidVotesAbs, unvalidVotesRel, 
+     authVotersAbs, authMaleVotersRel, authMaleVotersAbs, authFemaleVotersRel, authFemaleVotersAbs,
+     listplace, district, parish, parishDistrict, partyName, partyShortname, votes)
 
 #---                     ----
-####  OPEN & CHECK DATA  ####
+####  OPEN & REDUCE DATA  ####
 #---                     ----
 
 # open results of the districts and remove clutter
 rawVotesDistrict <- read.csv2(paste0( folderRaw, "/GRW2012_Sprengelbezerg.csv" ), fileEncoding = "iso-8859-1")
 votesDistrict <- rawVotesDistrict
-votesDistrict <- votesDistrict[, c("beznr", "ptname", "listenplatz", "stimmen")]
+votesDistrict <- votesDistrict[, c("beznr", "ptname", "listenplatz", "stimmen", "gueltig")]
   
 # open results of the parishes and remove clutter
 rawVotesParish <- read.csv2(paste0( folderRaw, "/GRW2012_Sprengelerg.csv" ), fileEncoding = "iso-8859-1")
 votesParish <- rawVotesParish
-votesParish <- votesParish[, c("sprengel", "ptname", "listenplatz", "stimmen")]
+votesParish <- votesParish[, c("sprengel", "ptname", "listenplatz", "stimmen", "gueltig")]
 
 # open authorized voters and remove clutter
 rawAvParish <- read.csv2(paste0( folderRaw, "/GRW2012_Wahlberechtigte.csv" ), fileEncoding = "iso-8859-1")
@@ -83,27 +92,28 @@ avParish <- avParish[, c("sprengel", "wahlbe_gesamt", "wahlbe_mann", "wahlbe_fra
 # Check data
 # CheckData(votesDistrict) DEBUG
 # CheckData(votesDistrict) DEBUG
+#
 # conclusion:
 # missing value short party name of Einsparkraftwerk
 # everything else okay
 
 #---
-####  TIDY DATA  ####
+####  PREPARE DATA  ####
 #---
 
 # correct parish & district columns
 # columns: bezirk, sprengel, bezirk_sprengel, gesamt, unguel, gueltig
 avParish <- SplitParishAndDistrict(avParish, 1)
 votesParish <- SplitParishAndDistrict(votesParish, 1)
-authorizedVotersDistrict <- rawVotesDistrict[, c("beznr", "gesamt", "unguel", "gueltig", "sprengelanzahl")]
-authorizedVotersParish <- rawVotesParish[, c("sprengel", "gesamt", "unguel", "gueltig")]
+authorizedVotersDistrict <- rawVotesDistrict[, c("beznr", "gesamt", "gueltig", "unguel", "sprengelanzahl")]
+authorizedVotersParish <- rawVotesParish[, c("sprengel", "gesamt", "gueltig", "unguel")]
 votesDistrict$beznr <- votesDistrict$beznr / 100
 authorizedVotersDistrict$beznr <- authorizedVotersDistrict$beznr / 100
 authorizedVotersParish <- SplitParishAndDistrict(authorizedVotersParish, 1)
 
 # delete duplicate rows
 authorizedVotersDistrict <- authorizedVotersDistrict[!duplicated(authorizedVotersDistrict),]
-rownames(authorizedVotersDistrict) <- 1:numDistricts
+rownames(authorizedVotersDistrict) <- 1:eleData[["numDistricts"]]
 authorizedVotersParish <- authorizedVotersParish[!duplicated(authorizedVotersParish),]
 
 # correct party short name of Einsparkraftwerk
@@ -115,8 +125,16 @@ levels(votesParish$ptname)[1] = "EKW"
 avParish <- cbind(avParish, authorizedVotersParish[, c("gesamt", "gueltig", "unguel")])
 
 
+#---
+####  SAVE DATA  ####
+#---
+save(rawAvParish, rawVotesDistrict, rawVotesParish, file="./data/raw_grw2012.rda")
+rm(rawAvParish, rawVotesDistrict, rawVotesParish)
+save(list=ls(), file="./data/prep_grw2012.rda")
+rm(list=ls())
+
 ##  FINAL DATA 
-#================================================
+#===============
 #
 # avParish 
 #   rows: ascending number
@@ -132,3 +150,4 @@ avParish <- cbind(avParish, authorizedVotersParish[, c("gesamt", "gueltig", "ung
 #   cols: district, parish, district and parish, short name party, list place, votes
 #
 #================================================
+
